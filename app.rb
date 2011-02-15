789fef4d0a79fd1e2c02d0128b0b646a92a863d4
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2011-02-15 10:23:14 +0100

    Updated config and paths to work with scss templates and with shorter paths

--- app.rb
+++ app.rb
@@ -14,9 +14,9 @@ configure do
 end
 
 # at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
-get '/stylesheets/:name.css' do
+get '/s/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
-  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+  scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
 end
 
 get '/' do
