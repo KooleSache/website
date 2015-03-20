1260898d61e9d7c47dd26c6b69877931dcd413a5
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-03-20 18:59:28 +0100

    Some CSS adjustements. More error prone JS
    Former-commit-id: 9629caed06b8d28da54d8b123c038eac8e88de37

--- js/index.js
+++ js/index.js
@@ -2,6 +2,11 @@ var forEach = Array.prototype.forEach;
 
 var onMouseEnter = function(event, el, videoEl) {
     console.log("Playing...");
+    var timeRanges = videoEl.played
+    if (timeRanges.length === 1) {
+        console.log(timeRanges.start(0));
+        console.log(timeRanges.end(0));
+    }
     videoEl.play()
 };
 
