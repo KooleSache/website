b67c606c9c9e435913bb7ab5a6653c3ecaed0638
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-03-29 22:40:46 +0200

    Split videos into shorter ones. Better handling of loading and playing progress via JS. Display playing progress.
    Former-commit-id: 361435d3765a6b1ec2e1b4b25512720c3894a4ea

--- js/index.js
+++ js/index.js
@@ -2,26 +2,45 @@ var forEach = Array.prototype.forEach;
 
 var onMouseEnter = function(event, el, videoEl) {
     console.log("Playing...");
-    console.log(videoEl);
-    var timeRanges = videoEl.played
-    if (timeRanges.length === 1) {
-        console.log(timeRanges.start(0));
-        console.log(timeRanges.end(0));
-    }
+    videoEl.classList.add("active");
     videoEl.play()
 };
 
 var onMouseLeave = function(event, el, videoEl) {
     console.log("Pausing...");
+    videoEl.classList.remove("active");
     videoEl.pause()
+};
+
+var onTimeUpdate = function(event, el, videoEl) {
+    var playedPercent = (videoEl.currentTime / videoEl.duration) * 100 ;
+    var progressEl = el.querySelector('.features__progress')
+    if (progressEl != null) {
+        progressEl.style.width = playedPercent + "%";
+    }
 };
 
 var initTourForContainer = function(id) {
     var container = document.getElementById(id);
-    var video = container.querySelector('video');
     var items = container.querySelectorAll('.features__item');
 
+
     forEach.call(items, function(item){
+        if (!item.id) {
+            return;
+        }
+
+        var name = item.id.split('-')[1];
+        var video = document.getElementById('video-' + name);
+
+        video.addEventListener('progress',function() {
+            if (this.buffered.length) {
+                console.log("Loading...", this.buffered.end(0) / this.duration);
+            }
+        });
+
+        video.addEventListener('timeupdate', onTimeUpdate.bind(this, event, item, video));
+
         item.addEventListener('mouseenter', onMouseEnter.bind(this, event, item, video));
         item.addEventListener('mouseleave', onMouseLeave.bind(this, event, item, video));
     });
