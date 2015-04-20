7501c67b844ae420570ccba7d4481cfe9dca0049
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-04-21 00:18:28 +0200

    Added tasks for compiling and deploying to production. Better webpack.config Use hash in production for cache busting.
    Former-commit-id: cf8257aec89cc540cc9b70776f4af39099fdc172

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,32 @@
 var path = require("path");
+var fs = require("fs");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var isProduction = process.env.NODE_ENV === 'production';
 var cssLoaders = ["style-loader", "css-loader", "autoprefixer-loader?browsers=last 2 version"];
 var scssLoaders = cssLoaders.concat(["sass-loader?imagePath=/_images&includePaths[]=./_sass"]);
+var plugins = [new ExtractTextPlugin("[name].[hash].css")];
 
 if (isProduction) {
     cssLoaders = ExtractTextPlugin.extract(cssLoaders.slice(1).join('!'));
     scssLoaders = ExtractTextPlugin.extract(scssLoaders.slice(1).join('!'));
+    console.log("Compiling for production!")
+    plugins = plugins.concat([
+        // Write a bundle compilation hash to application.properties to use it in HTML
+        function() {
+            this.plugin("done", function(stats) {
+                var jekyllConfig = fs.readFileSync(path.join(__dirname, "_config.yml"), { encoding: "utf-8" });
+                jekyllConfig = jekyllConfig.replace("production: false", "production: true")
+                fs.writeFileSync(
+                    path.join(__dirname, "_config_production.yml"),
+                    jekyllConfig
+                );
+                fs.appendFileSync(
+                    path.join(__dirname, "_config_production.yml"),
+                    "webpack_hash: " + stats.hash + "\n"
+                );
+            });
+        }
+    ]);
 } else {
     cssLoaders = cssLoaders.join('!');
     scssLoaders = scssLoaders.join('!');
@@ -16,13 +36,11 @@ module.exports = {
   entry: ["./_js/index.js"],
   output: {
     path: path.join(__dirname, "assets"),
-    publicPath: "assets/",
-    filename: "[name].[hash].js",
+    publicPath: "/assets/",
+    filename: isProduction ? "[name].[hash].js" : "[name].js",
     chunkFilename: "[name].[id].js"
   },
-  plugins: [
-      new ExtractTextPlugin("[name].[hash].css")
-  ],
+  plugins: plugins,
   module: {
     loaders: [
       {
