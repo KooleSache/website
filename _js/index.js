763e25b2f63ddb8e82b2bdacaefe7893e3865825
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-04-23 21:40:03 +0200

    Implemented interactive part of play button with buffering / playback state.
    Former-commit-id: 888ae7864560776e43fdfc7f210d436e5f1deb95

--- _js/index.js
+++ _js/index.js
@@ -1,24 +1,25 @@
 require('../_sass/main.scss');
 
-var forEach = Array.prototype.forEach;
+const CIRCLE_LENGTH = Math.PI * 45 * 2;
+const forEach = Array.prototype.forEach;
 
 var onMouseEnter = function(event, el, videoEl) {
-    console.log("Playing...");
+    //console.log("Playing...");
     videoEl.classList.add("active");
-    typeof videoEl.play === "function" && videoEl.play();
+    //typeof videoEl.play === "function" && videoEl.play();
 };
 
 var onMouseLeave = function(event, el, videoEl) {
-    console.log("Pausing...");
+    //console.log("Pausing...");
     videoEl.classList.remove("active");
-    typeof videoEl.pause === "function" && videoEl.pause();
-    videoEl.currentTime = 0; // Always go to the beginning
+    //typeof videoEl.pause === "function" && videoEl.pause();
+    //videoEl.currentTime = 0; // Always go to the beginning
 };
 
 var onTimeUpdate = function(event, el, videoEl) {
-    var playedPercent = (videoEl.currentTime / videoEl.duration) * 100 ;
-    var progressEl = el.querySelector('.js-progress')
-    if (progressEl != null) {
+    var playedPercent = (videoEl.currentTime / videoEl.duration) * 100;
+    var progressEl = el.querySelector('.js-progress');
+    if (progressEl) {
         progressEl.style.width = playedPercent + "%";
     }
 };
@@ -38,12 +39,6 @@ var initTourForContainer = (id) => {
         var name = item.id.split('-')[1];
         var video = document.getElementById('video-' + name);
 
-        video.addEventListener('progress',()=>{
-            if (this.buffered.length) {
-                console.log("Loading...", this.buffered.end(0) / this.duration);
-            }
-        });
-
         video.addEventListener('timeupdate', onTimeUpdate.bind(this, event, item, video));
         item.addEventListener('mouseenter', onMouseEnter.bind(this, event, item, video));
         item.addEventListener('mouseleave', onMouseLeave.bind(this, event, item, video));
@@ -57,19 +52,13 @@ var initVideoProgressInContainer = (id) => {
 
     var video = container.querySelector('video');
 
-    video.addEventListener('progress',()=>{
-        if (this.buffered.length) {
-            console.log("Loading...", this.buffered.end(0) / this.duration);
-        }
-    });
-
     video.addEventListener('timeupdate', onTimeUpdate.bind(this, event, container, video));
 };
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    //initVideoProgressInContainer('howto');
-    //initTourForContainer('loupeTour');
-    //initTourForContainer('overlayTour');
+    initVideoProgressInContainer('howto');
+    initTourForContainer('loupeTour');
+    initTourForContainer('overlayTour');
 
     forEach.call(document.querySelectorAll("#switch-theme input"), item => {
         item.addEventListener("change", event => {
@@ -81,17 +70,45 @@ document.addEventListener("DOMContentLoaded", ()=>{
     });
 
     // Videos pre-loading indication
-    const fullCircle = 565.48;
-    const progress = document.querySelector('svg .playButton__progress');
-
     forEach.call(document.querySelectorAll("video"), video => {
+        const id = video.id;
+        const playButtonEl = document.querySelector(`.playButton[data-video=${id}]`);
+        const bufferedProgressEl = playButtonEl ? playButtonEl.querySelector('.playButton__progress_buffer') : null;
+        const playedProgressEl = playButtonEl ? playButtonEl.querySelector('.playButton__progress_time') : null;
+
+        playButtonEl && playButtonEl.addEventListener('click', () => {
+            video.paused ? video.play() : video.pause();
+        });
+
         video.addEventListener('progress', (event) => {
             const videoEl = event.target;
             if (videoEl.buffered && videoEl.buffered.length) {
-                var percentLoaded = videoEl.buffered.end(0) / videoEl.duration
-                console.log("Loading...", percentLoaded);
-                progress.style.strokeDashoffset = fullCircle * (1 - percentLoaded);
+                const amount = videoEl.buffered.end(0) / videoEl.duration
+                if (bufferedProgressEl) {
+                    bufferedProgressEl.style.strokeDashoffset = CIRCLE_LENGTH * (1 - amount);
+                }
             }
-        })
+        });
+
+        video.addEventListener('timeupdate', (event) => {
+            const videoEl = event.target;
+            const amount = (videoEl.currentTime / videoEl.duration);
+            if (playedProgressEl) {
+                playedProgressEl.style.strokeDashoffset = CIRCLE_LENGTH * (1 - amount);
+            }
+        });
+
+        video.addEventListener('playing', () => {
+            playButtonEl && playButtonEl.classList.add("playButton_playing");
+        });
+
+        video.addEventListener('ended', () => {
+            playButtonEl && playButtonEl.classList.remove("playButton_playing");
+        });
+
+        video.addEventListener('pause', () => {
+            playButtonEl && playButtonEl.classList.remove("playButton_playing");
+        });
+
     });
 });
