3cc1fd3383351f21a9f014cae70d258576b2ed9c
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-02-11 21:48:10 +0100

    Use config namespace for options. Better webpackDevServer config
    Former-commit-id: e4655aa3cc195afb7332d16da7fa36d4f3539b1e

--- index.js
+++ index.js
@@ -58,27 +58,14 @@ if (!isProduction) {
         .use(webpackDevServer(webpackConfig, {
             port: 8081,
             contentBase: 'http://localhost:8081/',
-            // or: contentBase: 'http://localhost/',
-
             hot: true,
-            // Enable special support for Hot Module Replacement
-            // Page is no longer updated, but a 'webpackHotUpdate' message is send to the content
-            // Use 'webpack/hot/dev-server' as additional module in your entry point
-            // Note: this does _not_ add the `HotModuleReplacementPlugin` like the CLI option does.
-
-            // Set this if you want webpack-dev-server to delegate a single path to an arbitrary server.
-            // Use '*' to proxy all paths to the specified server.
-            // This is useful if you want to get rid of 'http://localhost:8080/' in script[src],
-            // and has many other use cases (see https://github.com/webpack/webpack-dev-server/pull/127 ).
             proxy: {
                 '*': 'http://localhost:8080'
             },
-
             // webpack-dev-middleware options
-            quiet: false,
+            quiet: true,
             noInfo: false,
             publicPath: 'http://localhost:8081/',
-            //headers: { 'X-Custom-Header': 'yes' },
             stats: {colors: true}
 
         }))
