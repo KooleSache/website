6c10313c1c2bbd0c0578b27ced1dcc498375ca85
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-02-12 11:57:54 +0100

    Use <em> instead of <strong>
    Former-commit-id: 6f9c8a207f5891780eeae661d6b003e5f3a5f93d

--- metadata.js
+++ metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
     site: {
         title: 'ColorSnapper',
-        description: 'The Mac OS X <strong>color picker</strong> app for designers <span class="amp">&amp;</span> developers which makes it easy to <strong>collect</strong>, <strong>adjust</strong>, <strong>organize</strong> and <strong>export</strong> colors of <strong>any pixel</strong> on the screen.',
+        description: 'The Mac OS X <em>color picker</em> app for designers <span class="amp">&amp;</span> developers which makes it easy to <em>collect</em>, <em>adjust</em>, <em>organize</em> and <em>export</em> colors of <em>any pixel</em> on the screen.',
         keywords: 'color picker mac, color picker, colorpicker, color, picker, mac, color value, application, app, osx, apple, pick retina pixel mac, HiDPI pixel, adjust, export, favorite, favourite, manage, retina, HiDPI, precision, gesture, shortcut, css, cocoa, rgb, hsl, hex, swift, uicolor, .NET, colour, pixel, web, screen, photoshop, illustrator, bgcolor, fgcolor, background, foreground',
         email: 'support@koolesache.com',
         twitter_username: 'colorsnapper',
