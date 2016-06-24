3026ef17f1dd30ace08e33cb7ce552aea4b664b0
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-06-24 11:51:50 +0200

    Use product IDs from metadata.js
    Former-commit-id: da48b71cce987e30769f0b27b842878879f764b2

--- metadata.js
+++ metadata.js
@@ -5,8 +5,9 @@ module.exports = {
         keywords: 'color picker mac, color picker, colorpicker, color, picker, mac, color value, application, app, osx, apple, pick retina pixel mac, HiDPI pixel, adjust, export, favorite, favourite, manage, retina, HiDPI, precision, gesture, shortcut, css, cocoa, rgb, hsl, hex, swift, uicolor, .NET, colour, pixel, web, screen, photoshop, illustrator, bgcolor, fgcolor, background, foreground',
         email: 'support@koolesache.com',
         twitter_username: 'colorsnapper',
-        mas_product_id: '',
+        mas_product_id: '969418666',
         paddle_product_id: '499167',
+        paddle_vendor_id: 9922,
         baseurl: '',
         url: 'http://colorsnapper.com'
     }
