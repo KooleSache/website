2ada4fe720fb8f1563288cf1d47b54fd63e75aca
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-06-24 11:53:24 +0200

    Removed console.logs
    Former-commit-id: 011202a78ab96900a82e1952fd4d83f9b62565de

--- _js/checkOSCompatibility.js
+++ _js/checkOSCompatibility.js
@@ -2,7 +2,5 @@ import easyPattern from 'easypattern'
 export default function checkOSCompatibility(ua) {
     const pattern = easyPattern('{*}mac os {*} {major}_{minor}_{patch}')
     const { major, minor } = pattern.match(ua)
-    console.log(major)
-    console.log(minor)
     return (major == 10 && minor >= 9) || (major >= 11)
 }
