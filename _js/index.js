4f3055fa08aa1d616b65a84d6b2736b3053c99ef
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2016-11-25 16:25:56 +0100

    Fixed eslint errors

--- _js/index.js
+++ _js/index.js
@@ -1,3 +1,4 @@
+/* eslint no-param-reassign: 0 */
 import '../_sass/_vars.scss'
 import '../_sass/_base.scss'
 import '../_sass/_layout.scss'
@@ -17,204 +18,202 @@ import '../_sass/changelog.scss'
 import '../_sass/main.scss'
 import checkOSCompatibility from './checkOSCompatibility'
 
-const CIRCLE_LENGTH = Math.PI * 45 * 2;
-const forEach = Array.prototype.forEach;
-let currentlyPlayingVideo = null;
+const CIRCLE_LENGTH = Math.PI * 45 * 2
+const forEach = Array.prototype.forEach
+let currentlyPlayingVideo = null
+
+function pauseVideoPlayback(video) {
+  if (video) video.pause()
+}
+
+function toggleVideoPlayback(video) {
+  if (video) {
+    if (video.paused) {
+      const playButtonEl = document.querySelector(`.features__item[data-video=${ video.id }] .playButton`)
+      if (playButtonEl) {
+        playButtonEl.classList.add('playButton_loading')
+      }
+      video.play()
+    } else {
+      video.pause()
+    }
+  }
+}
+
+function initGA(classNames) {
+  classNames.forEach((className) => {
+    forEach.call(document.querySelectorAll(`.${ className }`), (el) => {
+            // Display alert about incompatible OS X version
+            el && el.addEventListener('click', () => { // eslint-disable-line
+              if (className === 'buy-paddle' && !checkOSCompatibility(navigator.userAgent)) {
+                alert('Your OS version is not compatible with ColorSnapper2, which requires' +
+                      ' Mac OS X 10.9+ or macOS 11+.')
+              }
+            })
+
+            // Send tracking code
+            window.ga && window.ga('send', 'event', 'button', 'click', className) // eslint-disable-line
+    })
+  })
+}
 
 function onMouseEnter(video) {
     // Resume the video marked for that on hover
-    if (video &&
-        currentlyPlayingVideo !== video &&
-        video.wasPlaying) {
-        toggleVideoPlayback(currentlyPlayingVideo);
-        video.play();
-    }
+  if (video && currentlyPlayingVideo !== video && video.wasPlaying) {
+    toggleVideoPlayback(currentlyPlayingVideo)
+    video.play()
+  }
     // Toggle screenshots on mouse hover
-    video && video.classList.add("is-active");
+  if (video) video.classList.add('is-active')
 }
 
 function onMouseLeave(video) {
     // If the video is playing now we mark it to resume next time we hover it
-    if (video && video.played.length) {
-        video.wasPlaying = !video.paused;
-    }
+  if (video && video.played.length) {
+    video.wasPlaying = !video.paused
+  }
     // Toggle screenshots on mouse hover
-    if (video && video.paused) {
-        video.classList.remove("is-active");
-    }
+  if (video && video.paused) {
+    video.classList.remove('is-active')
+  }
 }
 
 function onTimeUpdate(el, video) {
-    var playedPercent = (video.currentTime / video.duration) * 100;
-    var progressEl = el.querySelector('.js-progress');
-    if (progressEl) {
-        progressEl.style.width = playedPercent + "%";
-    }
+  const playedPercent = (video.currentTime / video.duration) * 100
+  const progressEl = el.querySelector('.js-progress')
+  if (progressEl) {
+    progressEl.style.width = `${ playedPercent }%`
+  }
 }
 
 function initTourForContainer(id) {
-    var container = document.getElementById(id);
-    if (!container) return;
-    var links = container.querySelectorAll('.features__item[data-video]');
+  const container = document.getElementById(id)
+  if (!container) return
+  const links = container.querySelectorAll('.features__item[data-video]')
 
-    forEach.call(links, link => {
-        let videoID = link.getAttribute('data-video');
-        let video = document.getElementById(videoID);
+  forEach.call(links, (link) => {
+    const videoID = link.getAttribute('data-video')
+    const video = document.getElementById(videoID)
 
-        if (typeof video !== "undefined") {
-            video.addEventListener('timeupdate', onTimeUpdate.bind(this, link, video));
-            link.addEventListener('mouseenter', onMouseEnter.bind(this, video));
-            link.addEventListener('mouseleave', onMouseLeave.bind(this, video));
-            link.addEventListener('click', () => {
-                if (currentlyPlayingVideo && video !== currentlyPlayingVideo) {
-                    toggleVideoPlayback(currentlyPlayingVideo);
-                }
-                toggleVideoPlayback(video);
-            });
+    if (typeof video !== 'undefined') {
+      video.addEventListener('timeupdate', onTimeUpdate.bind(this, link, video))
+      link.addEventListener('mouseenter', onMouseEnter.bind(this, video))
+      link.addEventListener('mouseleave', onMouseLeave.bind(this, video))
+      link.addEventListener('click', () => {
+        if (currentlyPlayingVideo && video !== currentlyPlayingVideo) {
+          toggleVideoPlayback(currentlyPlayingVideo)
         }
-    });
+        toggleVideoPlayback(video)
+      })
+    }
+  })
 }
 
 function initVideoProgressInContainer(id) {
-    const container = document.getElementById(id);
-    if (!container) return;
-    const video = container.querySelector('video');
-    video.addEventListener('timeupdate', onTimeUpdate.bind(this, container, video));
+  const container = document.getElementById(id)
+  if (!container) return
+  const video = container.querySelector('video')
+  video.addEventListener('timeupdate', onTimeUpdate.bind(this, container, video))
 }
 
-function pauseVideoPlayback(video) {
-    video && video.pause();
-}
+document.addEventListener('DOMContentLoaded', () => {
+  initGA(['buy-mas', 'buy-paddle', 'buy-download'])
+  initVideoProgressInContainer('howto')
+  initTourForContainer('loupeTour')
+  initTourForContainer('overlayTour')
 
-function toggleVideoPlayback(video) {
-    if (video) {
+  forEach.call(document.querySelectorAll('#switch-theme input'), (item) => {
+    item.addEventListener('change', (event) => {
+      const themeName = event.target.value
+
+      forEach.call(document.querySelectorAll('#overlayTour .tour__img'), (img) => {
+        img.src = img.src.replace(/(dark|light)/, themeName)
+      })
+
+      forEach.call(document.querySelectorAll('#overlayTour .tour__video'), (video) => {
         if (video.paused) {
-            const playButtonEl = document.querySelector(`.features__item[data-video=${video.id}] .playButton`)
-            if (playButtonEl) {
-                playButtonEl.classList.add("playButton_loading");
-            }
-            video.play();
+          video.poster = video.poster.replace(/(dark|light)/, themeName)
+          video.src = video.src.replace(/(dark|light)/, themeName)
         } else {
-            video.pause();
-        }
-    }
-}
-
-function initGA(classNames) {
-    classNames.forEach(className => {
-        forEach.call(document.querySelectorAll(`.${className}`), (el) => {
-            // Display alert about incompatible OS X version
-            el && el.addEventListener('click', () => {
-                if (className === 'buy-paddle' && !checkOSCompatibility(navigator.userAgent)) {
-                    alert('Your OS version is not compatible with ColorSnapper2, which requires' +
-                        ' Mac OS X 10.9+ or macOS 11+.')
-                }
-            })
-
-            // Send tracking code
-            window.ga && window.ga('send', 'event', 'button', 'click', className);
-        });
-
-    })
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    initGA(['buy-mas', 'buy-paddle', 'buy-download']);
-    initVideoProgressInContainer('howto');
-    initTourForContainer('loupeTour');
-    initTourForContainer('overlayTour');
-
-    forEach.call(document.querySelectorAll("#switch-theme input"), item => {
-        item.addEventListener("change", event => {
-            const themeName = event.target.value;
-
-            forEach.call(document.querySelectorAll("#overlayTour .tour__img"), img => {
-                img.src = img.src.replace(/(dark|light)/, themeName);
-            });
-
-            forEach.call(document.querySelectorAll("#overlayTour .tour__video"), video => {
-                if (video.paused) {
-                    video.poster = video.poster.replace(/(dark|light)/, themeName);
-                    video.src = video.src.replace(/(dark|light)/, themeName);
-                } else {
                     // Stop all videos
-                    video.pause();
-                    video.wasPlaying = false;
-                    window.setTimeout(() => {
+          video.pause()
+          video.wasPlaying = false
+          window.setTimeout(() => {
                         // For some reason events won't fire if this code is sync
-                        video.currentTime = 0;
-                        window.setTimeout(() => {
+            video.currentTime = 0
+            window.setTimeout(() => {
                             // For some reason events won't fire if this code is sync
-                            video.poster = video.poster.replace(/(dark|light)/, themeName);
-                            video.src = video.src.replace(/(dark|light)/, themeName);
-                        }, 100);
-                    }, 100);
-                }
-            });
-        });
-    });
+              video.poster = video.poster.replace(/(dark|light)/, themeName)
+              video.src = video.src.replace(/(dark|light)/, themeName)
+            }, 100)
+          }, 100)
+        }
+      })
+    })
+  })
 
     // Videos pre-loading indication
-    forEach.call(document.querySelectorAll("video"), video => {
-        let id = video.id;
-        let featureEl = document.querySelector(`.features__item[data-video=${id}]`);
-        let playButtonEl = document.querySelector(`.playButton[data-video=${id}]`);
-        let bufferedProgressEl = playButtonEl ? playButtonEl.querySelector('.playButton__progress_buffer') : null;
-        let playedProgressEl = playButtonEl ? playButtonEl.querySelector('.playButton__progress_time') : null;
+  forEach.call(document.querySelectorAll('video'), (video) => {
+    const id = video.id
+    const featureEl = document.querySelector(`.features__item[data-video=${ id }]`)
+    const playButtonEl = document.querySelector(`.playButton[data-video=${ id }]`)
+    const bufferedProgressEl = playButtonEl ? playButtonEl.querySelector('.playButton__progress_buffer') : null
+    const playedProgressEl = playButtonEl ? playButtonEl.querySelector('.playButton__progress_time') : null
 
-        playButtonEl && playButtonEl.addEventListener('click', (event) => {
-            event.preventDefault();
-            event.stopPropagation();
+    if (playButtonEl) {
+      playButtonEl.addEventListener('click', (event) => {
+        event.preventDefault()
+        event.stopPropagation()
 
             // Stop currently playing video
-            if (video !== currentlyPlayingVideo) {
-                pauseVideoPlayback(currentlyPlayingVideo);
-            }
-
-            toggleVideoPlayback(video, playButtonEl);
-        });
-
-        function renderProgress(video) {
-            if (video.buffered && video.buffered.length) {
-                const buffered = video.buffered.end(0) / video.duration
-                if (bufferedProgressEl) {
-                    bufferedProgressEl.style.strokeDashoffset = CIRCLE_LENGTH * (1 - buffered);
-                }
-            }
-            const played = video.currentTime / video.duration;
-            if (playedProgressEl) {
-                playedProgressEl.style.strokeDashoffset = CIRCLE_LENGTH * (1 - played);
-            }
+        if (video !== currentlyPlayingVideo) {
+          pauseVideoPlayback(currentlyPlayingVideo)
         }
 
-        video.addEventListener('progress', event => {
-            renderProgress(event.target);
-        });
+        toggleVideoPlayback(video, playButtonEl)
+      })
+    }
 
-        video.addEventListener('timeupdate', event => {
-            renderProgress(event.target);
-        });
+    function renderProgress(videoEl) {
+      if (videoEl.buffered && videoEl.buffered.length) {
+        const buffered = videoEl.buffered.end(0) / videoEl.duration
+        if (bufferedProgressEl) {
+          bufferedProgressEl.style.strokeDashoffset = CIRCLE_LENGTH * (1 - buffered)
+        }
+      }
+      const played = videoEl.currentTime / videoEl.duration
+      if (playedProgressEl) {
+        playedProgressEl.style.strokeDashoffset = CIRCLE_LENGTH * (1 - played)
+      }
+    }
 
-        video.addEventListener('playing', () => {
-            currentlyPlayingVideo = video;
-            video.classList.add("is-active");
-            featureEl && featureEl.classList.add("is-active");
-            playButtonEl && playButtonEl.classList.remove("playButton_loading");
-            playButtonEl && playButtonEl.classList.add("playButton_playing");
-        });
+    video.addEventListener('progress', (event) => {
+      renderProgress(event.target)
+    })
 
-        video.addEventListener('pause', () => {
-            if (currentlyPlayingVideo === video) {
-                currentlyPlayingVideo = null;
-            }
-            video.classList.remove("is-active");
-            featureEl && featureEl.classList.remove("is-active");
-            playButtonEl && playButtonEl.classList.remove("playButton_playing");
-        });
+    video.addEventListener('timeupdate', (event) => {
+      renderProgress(event.target)
+    })
 
-        video.addEventListener('loadeddata', () => {
-            playButtonEl && playButtonEl.classList.remove("playButton_loading");
-        });
+    video.addEventListener('playing', () => {
+      currentlyPlayingVideo = video
+      video.classList.add('is-active')
+      if (featureEl) featureEl.classList.add('is-active')
+      if (playButtonEl) playButtonEl.classList.remove('playButton_loading')
+      if (playButtonEl) playButtonEl.classList.add('playButton_playing')
+    })
 
-    });
-});
+    video.addEventListener('pause', () => {
+      if (currentlyPlayingVideo === video) {
+        currentlyPlayingVideo = null
+      }
+      video.classList.remove('is-active')
+      if (featureEl) featureEl.classList.remove('is-active')
+      if (playButtonEl) playButtonEl.classList.remove('playButton_playing')
+    })
+
+    video.addEventListener('loadeddata', () => {
+      if (playButtonEl) playButtonEl.classList.remove('playButton_loading')
+    })
+  })
+})
--- index.js
+++ index.js
@@ -20,107 +20,112 @@ const config = require('./config')
 const webpackConfig = require('./webpack.config')
 
 function cb(json) {
-    const formatOptions = { year: 'numeric', month: 'long', day: 'numeric' }
-    return json.reduce((prev, item) => {
-        var versionNumber = item.tag_name.replace('v', '')
-        const filename = `changelog/${ versionNumber }.md`
-        return Object.assign(prev, {
-            [filename]: {
-                layout: 'page.html',
-                collection: 'changelog',
-                title: versionNumber,
-                dateString: new Date(item.created_at).toLocaleDateString('en', formatOptions),
-                date: new Date(item.created_at),
-                contents: new Buffer(item.body)
-            }
-        })
-    }, {})
+  const formatOptions = { year: 'numeric', month: 'long', day: 'numeric' }
+  return json.reduce((prev, item) => {
+    const versionNumber = item.tag_name.replace('v', '')
+    const filename = `changelog/${ versionNumber }.md`
+    return Object.assign(prev, {
+      [filename]: {
+        layout: 'page.html',
+        collection: 'changelog',
+        title: versionNumber,
+        dateString: new Date(item.created_at).toLocaleDateString('en', formatOptions),
+        date: new Date(item.created_at),
+        contents: new Buffer(item.body)
+      }
+    })
+  }, {})
 }
 
 const server = metalsmith(__dirname)
-    .source(config.source)
-    .destination(config.destination)
-    .metadata(Object.assign(metadata, {
-        config: Object.assign(config, {
-            production: isProduction
-        })
-    }))
-    .use(debug())
-    .use(define(config))
-    .use(remote({
-        url: 'https://api.github.com/repos/adanmayer/ColorSnapper2/releases',
-        headers: {
-            'Authorization': 'token c8930579e19220e1b8c39876476e06c94d7fa4c5'
-        },
-        transformOpts: cb
-    }))
-    .use(collections({
-        changelog: {
-            pattern: 'changelog/**/*.md',
-            sortBy: 'date',
-            reverse: true,
-            refer: false
-        }
-    }))
-    .use(markdown({
-        html: true,
-        typographer: true,
-        linkify: true
-    }))
-    .use(permalinks({
-        pattern: ':permalink'
-    }))
-    .use(assets({
-        source: './img', // relative to the working directory
-        destination: './img' // relative to the build directory
-    })).use(assets({
-        source: './app', // relative to the working directory
-        destination: './app' // relative to the build directory
-    }))
+  .source(config.source)
+  .destination(config.destination)
+  .metadata(Object.assign(metadata, {
+    config: Object.assign(config, {
+      production: isProduction
+    })
+  }))
+  .use(debug())
+  .use(define(config))
+  .use(remote({
+    url: 'https://api.github.com/repos/adanmayer/ColorSnapper2/releases',
+    headers: {
+      Authorization: 'token c8930579e19220e1b8c39876476e06c94d7fa4c5'
+    },
+    transformOpts: cb
+  }))
+  .use(collections({
+    changelog: {
+      pattern: 'changelog/**/*.md',
+      sortBy: 'date',
+      reverse: true,
+      refer: false
+    }
+  }))
+  .use(markdown({
+    html: true,
+    typographer: true,
+    linkify: true
+  }))
+  .use(permalinks({
+    pattern: ':permalink'
+  }))
+  .use(assets({
+    source: './img', // relative to the working directory
+    destination: './img' // relative to the build directory
+  }))
+  .use(assets({
+    source: './app', // relative to the working directory
+    destination: './app' // relative to the build directory
+  }))
 
 if (!isProduction) {
-    var myConfig = Object.assign({}, webpackConfig)
-    myConfig.devtool = 'source-maps'
-    myConfig.entry = myConfig.entry.concat([
-        'webpack-dev-server/client?' + config.webpackUrl,
-        'webpack/hot/dev-server'
-    ])
-    myConfig.output.publicPath = config.webpackUrl + '/'
-    myConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
+  const myConfig = Object.assign({}, webpackConfig)
+  myConfig.devtool = 'source-maps'
+  myConfig.entry = myConfig.entry.concat([
+    `webpack-dev-server/client?${ config.webpackUrl }`,
+    'webpack/hot/dev-server'
+  ])
+  myConfig.output.publicPath = `${ config.webpackUrl }/`
+  myConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
 
-    server
-        .use(watch({
-            paths: {
-                '${source}/**/*': true,
-                '_layouts/*': '**/*',
-                '_includes/*': '**/*'
-            }
-        }))
-        .use(serve())
-        .use(webpackDevServer(myConfig, {
-            port: config.webpackPort,
-            host: config.webpackHost,
-            hot: true,
-            quiet: true,
-            noInfo: true,
-            stats: {colors: true}
-        }))
+  server
+    .use(
+      watch({
+        paths: {
+          '${source}/**/*': true, // eslint-disable-line
+          '_layouts/*': '**/*',
+          '_includes/*': '**/*'
+        }
+      })
+    )
+    .use(serve())
+    .use(
+      webpackDevServer(myConfig, {
+        port: config.webpackPort,
+        host: config.webpackHost,
+        hot: true,
+        quiet: true,
+        noInfo: true,
+        stats: { colors: true }
+      })
+    )
 } else {
-    server
-        .use(msWebpack(webpackConfig))
+  server
+    .use(msWebpack(webpackConfig))
 }
 
 server
-    .use(inplace({
-        engine: 'handlebars',
-        partials: '_includes'
-    }))
-    .use(layouts({
-        engine: 'handlebars',
-        directory: '_layouts',
-        partials: '_includes'
-    }))
-    .build(function (err) {
-        if (err) throw err
-        console.log('Built web-site to ' + config.destination)
-    })
+  .use(inplace({
+    engine: 'handlebars',
+    partials: '_includes'
+  }))
+  .use(layouts({
+    engine: 'handlebars',
+    directory: '_layouts',
+    partials: '_includes'
+  }))
+  .build((err) => {
+    if (err) throw err
+    console.log(`Built web-site to ${ config.destination }`) // eslint-disable-line
+  })
