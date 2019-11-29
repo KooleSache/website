5b7f89eeb579caa43fa7552ccd30c0fd0c91f21f
Andrey Okonetchnikov <okonet@users.noreply.github.com>
2019-11-29 11:17:54 +0100

    fix: Use https for the url to fix mixed content warnings (#29)

--- metadata.js
+++ metadata.js
@@ -1,14 +1,16 @@
 module.exports = {
   site: {
     title: 'ColorSnapper',
-    description: 'The macOS <em>color picker</em> app for designers <span class="amp">&amp;</span> developers which makes it easy to <em>collect</em>, <em>adjust</em>, <em>organize</em> and <em>export</em> colors of <em>any pixel</em> on the screen.',
-    keywords: 'color picker mac, color picker, colorpicker, color, picker, mac, color value, application, app, osx, apple, pick retina pixel mac, HiDPI pixel, adjust, export, favorite, favourite, manage, retina, HiDPI, precision, gesture, shortcut, css, cocoa, rgb, hsl, hex, swift, uicolor, .NET, colour, pixel, web, screen, photoshop, illustrator, bgcolor, fgcolor, background, foreground',
+    description:
+      'The macOS <em>color picker</em> app for designers <span class="amp">&amp;</span> developers which makes it easy to <em>collect</em>, <em>adjust</em>, <em>organize</em> and <em>export</em> colors of <em>any pixel</em> on the screen.',
+    keywords:
+      'color picker mac, color picker, colorpicker, color, picker, mac, color value, application, app, osx, apple, pick retina pixel mac, HiDPI pixel, adjust, export, favorite, favourite, manage, retina, HiDPI, precision, gesture, shortcut, css, cocoa, rgb, hsl, hex, swift, uicolor, .NET, colour, pixel, web, screen, photoshop, illustrator, bgcolor, fgcolor, background, foreground',
     email: 'support@koolesache.com',
     twitter_username: 'colorsnapper',
     mas_product_id: '969418666',
     paddle_product_id: '499167',
     paddle_vendor_id: 9922,
     baseurl: '',
-    url: 'http://colorsnapper.com'
+    url: 'https://colorsnapper.com'
   }
 }
