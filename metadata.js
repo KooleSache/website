1538154068578d70de347cede53533680df6bd67
Andrey Okonetchnikov <andrey@okonet.ru>
2019-12-15 17:01:54 +0100

    Adjust copy to hopefully improve page ranking on Google

--- metadata.js
+++ metadata.js
@@ -2,7 +2,7 @@ module.exports = {
   site: {
     title: 'ColorSnapper',
     description:
-      'The macOS <em>color picker</em> app for designers <span class="amp">&amp;</span> developers which makes it easy to <em>collect</em>, <em>adjust</em>, <em>organize</em> and <em>export</em> colors of <em>any pixel</em> on the screen.',
+      'The color picker for Mac that makes it easy to <em>inspect</em>, <em>adjust</em>, <em>organize</em>, and <em>export</em> precise color values of <em>any pixel</em> on the screen.',
     keywords:
       'color picker mac, color picker, colorpicker, color, picker, mac, color value, application, app, osx, apple, pick retina pixel mac, HiDPI pixel, adjust, export, favorite, favourite, manage, retina, HiDPI, precision, gesture, shortcut, css, cocoa, rgb, hsl, hex, swift, uicolor, .NET, colour, pixel, web, screen, photoshop, illustrator, bgcolor, fgcolor, background, foreground',
     email: 'support@koolesache.com',
