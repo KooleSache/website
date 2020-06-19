1e249657e8855cdd3b12a7e9db86159d149be2cb
Andrey Okonetchnikov <okonet@users.noreply.github.com>
2020-06-19 10:51:51 +0200

    Update checkOSCompatibility.js

--- _js/checkOSCompatibility.js
+++ _js/checkOSCompatibility.js
@@ -3,5 +3,5 @@ import easyPattern from 'easypattern'
 export default function checkOSCompatibility(ua) {
   const pattern = easyPattern('{*}mac os {*} {major}_{minor}_{patch}')
   const { major, minor } = pattern.match(ua.toLowerCase())
-  return (Number(major) === 10 && Number(minor) >= 11) || (Number(major) >= 11)
+  return (Number(major) === 10 && Number(minor) >= 12) || (Number(major) >= 11)
 }
