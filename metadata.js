e06b1744fc3598cdbebae1c91b206531ebf3051b
Andrey Okonetchnikov <okonet@users.noreply.github.com>
2022-07-03 18:14:40 +0200

    Update metadata.js

--- metadata.js
+++ metadata.js
@@ -5,7 +5,7 @@ module.exports = {
       'The color picker for Mac that makes it easy to <em>inspect</em>, <em>adjust</em>, <em>organize</em>, and <em>export</em> precise color values of <em>any pixel</em> on the screen.',
     keywords:
       'color picker mac, color picker, colorpicker, color, picker, mac, color value, application, app, osx, apple, pick retina pixel mac, HiDPI pixel, adjust, export, favorite, favourite, manage, retina, HiDPI, precision, gesture, shortcut, css, cocoa, rgb, hsl, hex, swift, uicolor, .NET, colour, pixel, web, screen, photoshop, illustrator, bgcolor, fgcolor, background, foreground',
-    email: 'support@koolesache.com',
+    email: 'support@colorsnapper.com',
     twitter_username: 'colorsnapper',
     mas_product_id: '969418666',
     paddle_product_id: '499167',
